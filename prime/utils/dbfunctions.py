9aaded0295e95de5a9658f58fa684a606180f059
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-07-22 20:05:01 +0530

    Update dbfunctions.py

--- prime/utils/dbfunctions.py
+++ prime/utils/dbfunctions.py
@@ -871,37 +871,3 @@ async def get_rss_feeds_count() -> int:
     feeds = await feeds.to_list(length=10000000)
     return len(feeds)
 
-#urllock
-
-async def add_chat(chat_id):
-    stark = lockurl.find_one({"chat_id": chat_id})
-    if stark:
-        return False
-    else:
-        lockurl.insert_one({"chat_id": chat_id})
-        return True
-
-
-async def remove_chat(chat_id):
-    stark = lockurl.find_one({"chat_id": chat_id})
-    if not stark:
-        return False
-    else:
-        lockurl.delete_one({"chat_id": chat_id})
-        return True
-
-
-async def get_all_chats():
-    r = list(lockurl.find())
-    if r:
-        return r
-    else:
-        return False
-
-
-async def get_session(chat_id):
-    stark = lockurl.find_one({"chat_id": chat_id})
-    if not stark:
-        return False
-    else:
-        return stark
