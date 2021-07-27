663c23ab6e5f8eca47e21783748542003203aa27
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-07-27 20:23:47 +0530

    Update spam.py

--- prime/modules/spam.py
+++ prime/modules/spam.py
@@ -2,17 +2,31 @@ from pyrogram import filters
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
-from prime import LOG_GROUP_ID, SUDOERS, USERBOT_PREFIX, app, app2, arq, BOT_NAME
-from prime.core.decorators.permissions import adminsOnly
-from prime.modules.admin import list_admins, member_permissions
-from prime.modules.trust import get_spam_data
-from prime.modules.userbot import edit_or_reply
-from prime.utils.dbfunctions import (is_spam_detection_on,
+from wbb import LOG_GROUP_ID, SUDOERS, USERBOT_PREFIX, app, app2, arq
+from wbb.core.decorators.permissions import adminsOnly
+from wbb.modules.admin import list_admins, member_permissions
+from wbb.modules.trust import get_spam_data
+from wbb.modules.userbot import edit_or_reply
+from wbb.utils.dbfunctions import (is_spam_detection_on,
                                    spam_detection_off,
                                    spam_detection_on)
-from prime.utils.filter_groups import spam_protection_group
+from wbb.utils.filter_groups import spam_protection_group
 
 __MODULE__ = "AntiSpam"
+__HELP__ = """
+**Antispam helps protect your group from spam.**
+- It's still new so it might be a bit inaccurate.
+- Currently, it only alerts for spam and gives you a button to delete the message,
+  but in the future, it will delete the spam message automatically.
+**Commands:**
+    - /spam
+        To mark a message as spam, this will help devs to
+        improve spam protection algorithm.
+    - /spam_detection [ENABLE|DISABLE]
+        To turn off spam detection in your chat.
+**Plot Twist:** NO, WE'RE NOT USING INTELLIVOID'S SPAM DETECTION API!
+"""
+
 
 @app.on_message(
     (filters.text | filters.caption) & ~filters.private & ~filters.me,
@@ -38,22 +52,22 @@ async def spam_protection_func(_, message: Message):
         return
     if user.id in (await list_admins(chat_id)):
         return
-    return
     text = f"""
 🚨  **SPAM DETECTED**  🚨
 **User:** {user.mention}
 **Message:** [Link]({message.link})
 **Spam Probability:** {data.spam_probability} %
 **Action:** Alerted
+**For To turn off :** __/spam_detection disable__.
 """
     keyboard = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text="Yes it's Spam", callback_data="s_p_spam"
+                    text="Yes it's spam", callback_data="s_p_spam"
                 ),
                 InlineKeyboardButton(
-                    text="No, it's not Spam",
+                    text="No, it's not spam",
                     callback_data="s_p_ham",
                 ),
             ],
@@ -67,7 +81,7 @@ async def spam_protection_func(_, message: Message):
 
 
 dev_forward = (
-    f"If you're not a developer of {BOT_NAME}, forward this message to devs, "
+    "If you're not a developer of **@Joker_ProRoBot**, forward this message to devs, "
     + "so that they can use it to improve spam protection algorithm."
 )
 
@@ -115,34 +129,34 @@ async def spam_p_callback(_, cq: CallbackQuery):
         )
 
 
-@app.on_message(filters.command("addprofanity") & ~filters.private)
+@app.on_message(filters.command("spam") & ~filters.private)
 async def spam_flag_func(_, message: Message):
     if not message.reply_to_message:
         return await message.reply_text(
-            "Reply to a message to flag it as __**#PROFANIT**__"
+            "Reply to a message to flag it as spam"
         )
 
     r = message.reply_to_message
     if not r.text and not r.caption:
         return await message.reply_text(
-            "Reply to a text message to flag it as __**#PROFANITY**__"
+            "Reply to a text message to flag it as spam"
         )
 
     text = r.text or r.caption
     if not text:
         return await message.reply_text(
-            "Reply to a text message to flag it as __**#PROFANITY**__"
+            "Reply to a text message to flag it as spam"
         )
 
     msg = f"""
-**ADMINS OF {message.chat.id} FLAGGED THIS MESSAGE AS __**#PROFANITY**__. [Suggestion]**
-
+**ADMINS OF {message.chat.id} FLAGGED THIS MESSAGE AS SPAM. [Suggestion]**
+```
 {text.markdown}
-
+```
 __{dev_forward}__
 """
     await message.reply_text(
-        "Message was flagged as __**#PROFANITY**__, Devs will use it to improve __**#PROFANITY**__ protection algorithm."
+        "Message was flagged as spam, Devs will use it to improve spam protection algorithm."
     )
     await app.send_message(
         LOG_GROUP_ID, msg, disable_web_page_preview=True
