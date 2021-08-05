21ee1a702f56436a24725238a3bf566b4e4e21e5
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-08-05 15:13:47 +0530

    Update dbfunctions.py

--- prime/utils/dbfunctions.py
+++ prime/utils/dbfunctions.py
@@ -908,3 +908,39 @@ async def get_session(chat_id):
         return False
     else:
         return stark
+
+def get_url(message_1: Message) -> Union[str, None]:
+    messages = [message_1]
+
+    if message_1.reply_to_message:
+        messages.append(message_1.reply_to_message)
+
+    text = ""
+    offset = None
+    length = None
+
+    for message in messages:
+        if offset:
+            break
+
+        if message.entities:
+            for entity in message.entities:
+                if entity.type == "url":
+                    text = message.text or message.caption
+                    offset, length = entity.offset, entity.length
+                    break
+
+    if offset in (None,):
+        return None
+
+    return text[offset : offset + length]
+
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+    return data
